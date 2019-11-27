5e1e0dce1b455835f13a6e0c84895329e8cb5b85
Johan Nordberg <its@johan-nordberg.com>
2019-11-27 22:18:13 +0900

    Add convenience StringCodingKey

--- Sources/EOSIO/Extensions/Codable+Convenience.swift
+++ Sources/EOSIO/Extensions/Codable+Convenience.swift
@@ -5,3 +5,23 @@ extension KeyedDecodingContainer {
         return try self.decode(T.self, forKey: key)
     }
 }
+
+internal struct StringCodingKey: CodingKey, ExpressibleByStringLiteral {
+    var stringValue: String
+    var intValue: Int? { return nil }
+    init(_ name: String) {
+        self.stringValue = name
+    }
+
+    init(stringLiteral value: String) {
+        self.stringValue = value
+    }
+
+    init?(stringValue: String) {
+        self.stringValue = stringValue
+    }
+
+    init?(intValue _: Int) {
+        return nil
+    }
+}
